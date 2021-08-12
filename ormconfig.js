787d2c9fadd8245bf5cf3de6489ba3fc325d600b
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2021-08-12 20:32:04 +0200

    feat: add typeorm

---
+++ ormconfig.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    // "url": process.env.DATABASE_URL,
+    "database": './db.sqlite3',
+    "type": process.env.DATABASE_TYPE || 'sqlite',
+    "synchronize": true,
+    "logging": false,
+    "entities": [
+        "src/entity/**/*.ts"
+    ],
+    "migrations": [
+        "src/migration/**/*.ts"
+    ],
+    "subscribers": [
+        "src/subscriber/**/*.ts"
+    ],
+    "cli": {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration",
+        "subscribersDir": "src/subscriber"
+    }
+};
